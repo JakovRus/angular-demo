9d439e3222815818e18f67d6f37d9126a81df502
Yakov Rogov <rogov@haulmont.com>
2020-02-08 14:19:17 +0400

    added shipping component; added http client module to app module to fetch shipping prices

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.sass']
 })
 export class CartComponent implements OnInit {
+  items;
 
-  constructor() { }
+  constructor(
+    private cartServise: CartService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.items = this.cartServise.getItems();
   }
 
 }
